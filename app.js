b617d3b04950c1be604664d5fff878fc585ae0bd
Opilane TTHK <opilane@tthk.ee>
2021-10-19 10:42:02 +0300

    s

---
+++ app.js
@@ -0,0 +1,18 @@
+const currentImage = document.querySelector('#image');
+const images = ['images/pilt1.jpg','images/pilt2.jpg','images/pilt3.jpg'];
+
+
+function changeImage() {
+    console.log("Hello from script file.")
+
+    let randomnumber = Math.floor(Math.random() * images.length);
+    
+    console.log([randomNumber]);
+
+    currentimage src = images[randomNumber];
+    
+
+
+
+
+}
